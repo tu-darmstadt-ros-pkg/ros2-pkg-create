72ff259061d3cd16f0fb9f7c872416fd30df1f01
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-09-20 23:52:52 +0200

    templatify header

--- ros2_cpp_pkg/{{ cookiecutter.package_name }}/include/{{ cookiecutter.package_name }}/{{ cookiecutter.node_name }}.hpp
+++ ros2_cpp_pkg/{{ cookiecutter.package_name }}/include/{{ cookiecutter.package_name }}/{{ cookiecutter.node_name }}.hpp
@@ -1,16 +1,7 @@
-#include <memory>
-
-#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
+{% if cookiecutter.has_action_server %}
 #include <rclcpp_action/rclcpp_action.hpp>
-#include <template_interfaces_pkg/action/template_action.hpp>
-#include <template_interfaces_pkg/msg/template_message.hpp>
-#include <template_interfaces_pkg/srv/template_service.hpp>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2/exceptions.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/transform_listener.h>
+{% endif %}
 
 
 namespace {{ cookiecutter.package_name }} {
@@ -19,83 +10,82 @@ namespace {{ cookiecutter.package_name }} {
 class {{ cookiecutter.node_cpp_class_name }} : public rclcpp::Node {
 
  public:
+
   {{ cookiecutter.node_cpp_class_name }}();
 
- protected:
-  // constants
-  static const std::string kInputTopic;
-  static const std::string kOutputTopic;
+ private:
 
-  // parameter names
-  static const std::string kPeriodParam;
+{% if cookiecutter.has_subscriber %}
+  static const std::string kInputTopic;
+{% endif %}
+{% if cookiecutter.has_publisher %}
+  static const std::string kOutputTopic;
+{% endif %}
+{% if cookiecutter.has_params %}
+  static const std::string kParam;
+{% endif %}
 
  private:
+
+{% if cookiecutter.has_params %}
   void loadParameters();
 
+{% endif %}
   void setup();
 
-  rcl_interfaces::msg::SetParametersResult parametersCallback(
-    const std::vector<rclcpp::Parameter>& parameters);
+{% if cookiecutter.has_params %}
+  rcl_interfaces::msg::SetParametersResult parametersCallback(const std::vector<rclcpp::Parameter>& parameters);
 
-  void publishTimerCallback();
-
+{% endif %}
+{% if cookiecutter.has_subscriber %}
   void topicCallback(const template_interfaces_pkg::msg::TemplateMessage& msg);
 
-  void serviceCallback(
-    const std::shared_ptr<
-      template_interfaces_pkg::srv::TemplateService::Request>
-      request,
-    std::shared_ptr<template_interfaces_pkg::srv::TemplateService::Response>
-      response);
-
-{% if cookiecutter.has_action_server %}
-  rclcpp_action::GoalResponse actionHandleGoal(
-    const rclcpp_action::GoalUUID& uuid,
-    std::shared_ptr<const {{ cookiecutter.action }}::Goal>
-      goal);
-
-  rclcpp_action::CancelResponse actionHandleCancel(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<
-      {{ cookiecutter.action }}>>
-      goal_handle);
-
-  void actionHandleAccepted(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<
-      {{ cookiecutter.action }}>>
-      goal_handle);
-
-  void actionExecute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<
-                       {{ cookiecutter.action }}>>
-                       goal_handle);
 {% endif %}
+{% if cookiecutter.has_service_server %}
+  void serviceCallback(const std::shared_ptr<template_interfaces_pkg::srv::TemplateService::Request>request, std::shared_ptr<template_interfaces_pkg::srv::TemplateService::Response> response);
 
+{% endif %}
+{% if cookiecutter.has_action_server %}
+  rclcpp_action::GoalResponse actionHandleGoal(const rclcpp_action::GoalUUID& uuid,std::shared_ptr<const template_interfaces_pkg::action::TemplateAction::Goal> goal);
+  rclcpp_action::CancelResponse actionHandleCancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<template_interfaces_pkg::action::TemplateAction>> goal_handle);
+  void actionHandleAccepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<template_interfaces_pkg::action::TemplateAction>> goal_handle);
+  void actionExecute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<template_interfaces_pkg::action::TemplateAction>> goal_handle);
+
+{% endif %}
+{% if cookiecutter.has_timer %}
+  void publishTimerCallback();
+
+{% endif %}
  private:
+
+{% if cookiecutter.has_params %}
   OnSetParametersCallbackHandle::SharedPtr parameters_callback_;
 
+{% endif %}
+{% if cookiecutter.has_subscriber %}
+  rclcpp::Subscription<template_interfaces_pkg::msg::TemplateMessage>::SharedPtr subscriber_;
+
+{% endif %}
+{% if cookiecutter.has_publisher %}
+  rclcpp::Publisher<template_interfaces_pkg::msg::TemplateMessage>::SharedPtr publisher_;
+
+{% endif %}
+{% if cookiecutter.has_service_server %}
+  rclcpp::Service<template_interfaces_pkg::srv::TemplateService>::SharedPtr service_server_;
+
+{% endif %}
+{% if cookiecutter.has_action_server %}
+  rclcpp_action::Server<template_interfaces_pkg::action::TemplateAction>::SharedPtr action_server_;
+
+{% endif %}
+{% if cookiecutter.has_timer %}
   rclcpp::TimerBase::SharedPtr publish_timer_;
 
-  rclcpp::Publisher<template_interfaces_pkg::msg::TemplateMessage>::SharedPtr
-    publisher_;
-
-  rclcpp::Subscription<template_interfaces_pkg::msg::TemplateMessage>::SharedPtr
-    subscriber_;
-
-  rclcpp::Service<template_interfaces_pkg::srv::TemplateService>::SharedPtr
-    service_server_;
-
-{% if cookiecutter.has_action_server %}
-  rclcpp_action::Server<
-    {{ cookiecutter.action }}>::SharedPtr action_server_;
 {% endif %}
-
-  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-
   double period_ = 1.0;
 
   int count_ = 0;
 };
 
 
-}  // namespace {{ cookiecutter.package_name }}
+}
