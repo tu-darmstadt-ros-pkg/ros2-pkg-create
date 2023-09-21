d553c12f1fdc2d4a2c2450397681a96768f3c234
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-09-21 18:48:30 +0200

    create very basic dummy python wrapper

---
+++ ros2-pkg-create/src/ros2_pkg_create/__init__.py
@@ -0,0 +1,2 @@
+__name__ = "ros2-pkg-create"
+__version__ = "0.0.1"
---
+++ ros2-pkg-create/src/ros2_pkg_create/__main__.py
@@ -0,0 +1,11 @@
+import copier
+
+
+def main():
+
+    print("Hello world")
+    # copier.run_copy("https://gitlab.ika.rwth-aachen.de/fb-fi/ops/templates/ros2/package_generator.git/ros2_cpp_pkg", ".")
+
+
+if __name__ == "__main__":
+    main()
