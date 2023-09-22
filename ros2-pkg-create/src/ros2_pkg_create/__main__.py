8d8889c7289459b98b928a3ae1321172a19eda99
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-09-22 13:58:15 +0200

    make ros2-pkg-create script use copier template from gitlab

--- ros2-pkg-create/src/ros2_pkg_create/__main__.py
+++ ros2-pkg-create/src/ros2_pkg_create/__main__.py
@@ -3,8 +3,7 @@ import copier
 
 def main():
 
-    print("Hello world")
-    # copier.run_copy("https://gitlab.ika.rwth-aachen.de/fb-fi/ops/templates/ros2/package_generator.git/ros2_cpp_pkg", ".")
+    copier.run_copy("https://gitlab.ika.rwth-aachen.de/fb-fi/ops/templates/ros2/package_generator.git", ".", unsafe=True, vcs_ref="pipeline")
 
 
 if __name__ == "__main__":
