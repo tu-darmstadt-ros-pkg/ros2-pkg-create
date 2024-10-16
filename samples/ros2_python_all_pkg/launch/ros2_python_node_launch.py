07620d8d37670ee3da4ba4fe09d49056ae6be080
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2024-10-16 21:45:35 +0200

    add log level arg to launch files

--- samples/ros2_python_all_pkg/launch/ros2_python_node_launch.py
+++ samples/ros2_python_all_pkg/launch/ros2_python_node_launch.py
@@ -15,6 +15,7 @@ def generate_launch_description():
         DeclareLaunchArgument("name", default_value="ros2_python_node", description="node name"),
         DeclareLaunchArgument("namespace", default_value="", description="node namespace"),
         DeclareLaunchArgument("params", default_value=os.path.join(get_package_share_directory("ros2_python_all_pkg"), "config", "params.yml"), description="path to parameter file"),
+        DeclareLaunchArgument("log_level", default_value="info", description="ROS logging level (debug, info, warn, error, fatal)"),
     ]
 
     nodes = [
@@ -24,6 +25,7 @@ def generate_launch_description():
             namespace=LaunchConfiguration("namespace"),
             name=LaunchConfiguration("name"),
             parameters=[LaunchConfiguration("params")],
+            arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
             output="screen",
             emulate_tty=True,
         )
