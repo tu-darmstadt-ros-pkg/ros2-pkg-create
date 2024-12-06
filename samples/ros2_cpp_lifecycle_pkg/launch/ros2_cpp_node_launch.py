6c8c75a81e7c68f75b4cbdf918267122e0aab391
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2024-12-06 11:30:43 +0100

    fix order of use_sim_time param

--- samples/ros2_cpp_lifecycle_pkg/launch/ros2_cpp_node_launch.py
+++ samples/ros2_cpp_lifecycle_pkg/launch/ros2_cpp_node_launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
     ]
 
     return LaunchDescription([
-        SetParameter("use_sim_time", LaunchConfiguration("use_sim_time")),
         *args,
+        SetParameter("use_sim_time", LaunchConfiguration("use_sim_time")),
         *nodes,
     ])
